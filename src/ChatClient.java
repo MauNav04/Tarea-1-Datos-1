304339379f5a0c8d9581c13621b8228cca5f60aa
Mauro Navarro <50973343+MauNav04@users.noreply.github.com>
2020-02-25 02:34:14 -0600

    Clases cliente y servidor
    Clases cliente y servidor funcionales. Servidor se aloja en un puerto y escucha, mientras que cliente se conecta y envía un mensaje.

---
+++ src/ChatClient.java
@@ -0,0 +1,52 @@
+import java.net.*;
+import java.io.*;
+
+public class ChatClient
+{  private Socket socket              = null;
+    private DataInputStream  console   = null;
+    private DataOutputStream streamOut = null;
+
+    public ChatClient(InetAddress serverName, int serverPort)
+    {  System.out.println("Establishing connection. Please wait ...");
+        try
+        {  socket = new Socket(serverName, serverPort);
+            System.out.println("Connected: " + socket);
+            start();
+        }
+        catch(UnknownHostException uhe)
+        {  System.out.println("Host unknown: " + uhe.getMessage());
+        }
+        catch(IOException ioe)
+        {  System.out.println("Unexpected exception: " + ioe.getMessage());
+        }
+        String line = "";
+        try
+        {  line = console.readLine();
+            streamOut.writeUTF(line);
+            streamOut.flush();
+        }
+        catch(IOException ioe)
+        {  System.out.println("Sending error: " + ioe.getMessage());
+        }
+
+        System.out.println("message sent...connection finished");
+    }
+    public void start() throws IOException
+    {  console   = new DataInputStream(System.in);
+        streamOut = new DataOutputStream(socket.getOutputStream());
+    }
+    public void stop()
+    {  try
+    {  if (console   != null)  console.close();
+        if (streamOut != null)  streamOut.close();
+        if (socket    != null)  socket.close();
+    }
+    catch(IOException ioe)
+    {  System.out.println("Error closing ...");
+    }
+    }
+    public static void main(String args[]) throws UnknownHostException {
+        ChatClient client = null;
+        client = new ChatClient(InetAddress.getLocalHost(), 9876);
+    }
+}
